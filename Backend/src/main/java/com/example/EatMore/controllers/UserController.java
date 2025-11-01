c85e35fc4f9bd5f48d80a717edf645dc5f57322f
Neeraj-kurmi <neerajkurmi2002@gmail.com>
2025-11-01 15:07:07 +0530

    security Enabled

--- Backend/src/main/java/com/example/EatMore/controllers/UserController.java
+++ Backend/src/main/java/com/example/EatMore/controllers/UserController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Map;
 
 @RestController
+@RequestMapping("/user")
 //@CrossOrigin(origins = "http://localhost:5173")
 public class UserController {
 
